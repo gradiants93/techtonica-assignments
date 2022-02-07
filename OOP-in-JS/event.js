d60e9ad35fbca0abfcc0b5b37c798b112dce358d
gradiants93 <75824980+gradiants93@users.noreply.github.com>
2022-02-07 14:03:58 -0800

    added indv events to eventArray

--- OOP-in-JS/event.js
+++ OOP-in-JS/event.js
@@ -5,3 +5,9 @@ class Event {
       this.availableTickets = [];
     }
 }
+const eventObj1 = new Event ('KLOS Golden Gala', 'An evening with hollywood vampires');
+const eventObj2 = new Event('Skillet & Sevendust', 'Victorious war tour');
+const eventObj3 = new Event('Jenny Lewis', 'On the line tour 2019');
+const eventArray = new Array();
+eventArray.push(eventObj1, eventObj2, eventObj3);
+console.log(eventArray)
