cd4c69d8f81375708556efbbd1490b2df6e11045
gradiants93 <75824980+gradiants93@users.noreply.github.com>
2022-02-07 16:09:52 -0800

    fixed DOM

--- OOP-in-JS/event.js
+++ OOP-in-JS/event.js
@@ -14,22 +14,24 @@ class Event {
         };
         return holder;
     }
-    searchTickets(minPrice, maxPrice) {
-        let holder = "Eligible tickets:";
-        let hasThings;
-        for (let i = 0; i < this.availableTickets.length; i++) {
-            let count = 1;
-            if ((this.availableTickets[i][1]> minPrice) && (this.availableTickets[i][1]< maxPrice)) {
-                holder += ` ${count}. ${this.availableTickets[i][0]} ($${this.availableTickets[i][1]})`;
-                count++;
-                hasThings++;
-            }
-        }
-        if (hasThings.length > 0) {
-            return holder;
-        }
-        return "No tickets available";
-    }
+    // Not quite working right now, it's returning just no tickets so I think there is a syntax error that is there
+
+    // searchTickets(minPrice, maxPrice) {
+    //     let holder = "Eligible tickets:";
+    //     let hasThings;
+    //     for (let i = 0; i < this.availableTickets.length; i++) {
+    //         let count = 1;
+    //         if ((this.availableTickets[i][1]> minPrice) && (this.availableTickets[i][1]< maxPrice)) {
+    //             holder += ` ${count}. ${this.availableTickets[i][0]} ($${this.availableTickets[i][1]})`;
+    //             count++;
+    //             hasThings++;
+    //         }
+    //     }
+    //     if (hasThings.length > 0) {
+    //         return holder;
+    //     }
+    //     return "No tickets available";
+    // }
 }
 class TicketType {
     constructor(name, price) {
@@ -59,10 +61,10 @@ console.log(eventObj1.allTickets())
 
 console.log(eventObj1.searchTickets(98, 300))
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     let html = '';
-//     eventArray.forEach((item) => {
-//         html += `<li>${item.name} - ${item.description} - ${item.allTickets()}`;
-//     });
-//     document.querySelector('#event').innerHTML = html;
-// });
+let eventDOM = document.getElementById('event').addEventListener('DOMContentLoaded', () => {
+    let html = '';
+    eventArray.forEach((item) => {
+        html += `<li>${item.name} - ${item.description} - ${item.allTickets()}`;
+    });
+    document.querySelector('#event').innerHTML = html;
+});
