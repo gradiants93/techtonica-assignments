66f985c32e23726e3f731789c1fd29c586c9b040
gradiants93 <75824980+gradiants93@users.noreply.github.com>
2022-02-07 17:40:08 -0800

    fixed searchTickets method and uncommented it

--- OOP-in-JS/event.js
+++ OOP-in-JS/event.js
@@ -1,18 +1,18 @@
-let eventSlot = document.getElementById('event');
+// let eventSlot = document.getElementById('event');
 
-let interval = setInterval(function() {
-    if(document.readyState === 'complete') {
-        clearInterval(interval);
-       // done();
+// let interval = setInterval(function() {
+//     if(document.readyState === 'complete') {
+//         clearInterval(interval);
+//        // done();
 
-        let html = '';
-        console.log("at beginning ", eventArray)
-        eventArray.forEach((item) => {
-            html += `<li>${item.name} - ${item.description} - ${item.allTickets()}`;
-        });
-        document.querySelector('#event').innerHTML = html;
-    }    
-}, 100);
+//         let html = '';
+//         console.log("at beginning ", eventArray)
+//         eventArray.forEach((item) => {
+//             html += `<li>${item.name} - ${item.description} - ${item.allTickets()}`;
+//         });
+//         document.querySelector('#event').innerHTML = html;
+//     }    
+// }, 100);
 
 class Event {
     constructor(name, description) {
@@ -32,22 +32,25 @@ class Event {
     }
     // Not quite working right now, it's returning just no tickets so I think there is a syntax error that is there
 
-    // searchTickets(minPrice, maxPrice) {
-    //     let holder = "Eligible tickets:";
-    //     let hasThings;
-    //     for (let i = 0; i < this.availableTickets.length; i++) {
-    //         let count = 1;
-    //         if ((this.availableTickets[i][1]> minPrice) && (this.availableTickets[i][1]< maxPrice)) {
-    //             holder += ` ${count}. ${this.availableTickets[i][0]} ($${this.availableTickets[i][1]})`;
-    //             count++;
-    //             hasThings++;
-    //         }
-    //     }
-    //     if (hasThings.length > 0) {
-    //         return holder;
-    //     }
-    //     return "No tickets available";
-    // }
+    searchTickets(minPrice, maxPrice) {
+        let holder = "Eligible tickets:";
+        let hasThings = 0;
+        let count = 1;
+        for (let i = 0; i < this.availableTickets.length; i++) {
+            if ((this.availableTickets[i][1] >= minPrice) && (maxPrice >= this.availableTickets[i][1])) {
+                holder += ` ${count}. ${this.availableTickets[i][0]} ($${this.availableTickets[i][1]})`;
+                count += 1;
+                hasThings += 1;
+                // console.log(` ${i+1}. ${this.availableTickets[i][0]} ($${this.availableTickets[i][1]})`, (this.availableTickets[i][1] >= minPrice), (maxPrice >= this.availableTickets[i][1]))
+            }
+        }
+       // console.log(holder, hasThings, "break point")
+        if (hasThings > 0) {
+            return holder;
+        } else {
+        return "No tickets available";
+        }
+    }
 }
 class TicketType {
     constructor(name, price) {
@@ -60,7 +63,7 @@ let eventObj2 = new Event('Skillet & Sevendust', 'Victorious war tour');
 let eventObj3 = new Event('Jenny Lewis', 'On the line tour 2019');
 let eventArray = new Array();
 eventArray.push(eventObj1, eventObj2, eventObj3);
-console.log(eventArray)
+// console.log(eventArray)
 
 eventObj1.addAvailableTickets("human", 299);
 eventObj1.addAvailableTickets("vampire", 99);
@@ -70,11 +73,13 @@ eventObj2.addAvailableTickets("Floor Seating", 80);
 eventObj3.addAvailableTickets("Orchestra", 300);
 eventObj3.addAvailableTickets("Mezzanine", 200);
 eventObj3.addAvailableTickets("Balcony", 100);
-console.log(eventObj1.availableTickets);
-console.log(eventObj2.availableTickets);
-console.log(eventObj3.availableTickets);
-console.log(eventObj1.allTickets())
+// console.log(eventObj1.availableTickets);
+// console.log(eventObj2.availableTickets);
+// console.log(eventObj3.availableTickets);
+// console.log(eventObj1.allTickets())
 
-//console.log(eventObj1.searchTickets(98, 300))
-
-
+// console.log(eventObj1.searchTickets(98, 300)) // Eligible tickets: 1. human ($299) 1. vampire ($99)
+// console.log(eventObj1.searchTickets(99, 299)) // Eligible tickets: 1. human ($299) 1. vampire ($99)
+// console.log(eventObj1.searchTickets(100, 298)) // No tickets available
+// console.log(eventObj1.searchTickets(99, 298)) // Eligible tickets: 1. vampire ($99)
+// console.log(eventObj1.searchTickets(100, 299)) // Eligible tickets: 1. human ($299)
