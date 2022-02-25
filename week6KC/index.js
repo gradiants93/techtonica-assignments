d823db70d317e42d79742af95dc11e8b5affc5f2
gradiants93 <75824980+gradiants93@users.noreply.github.com>
2022-02-25 13:59:28 -0800

    base routing

---
+++ week6KC/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 8080;
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'))
+});
+app.get('/about', function(req, res) {
+    res.sendFile(path.join(__dirname, '/about.html'))
+});
+app.get('/contactme', function(req, res) {
+    res.sendFile(path.join(__dirname, '/contactme.html'))
+});
+
+app.listen(port, function() {
+    console.log(`Listening on post ${port}`);
+});
